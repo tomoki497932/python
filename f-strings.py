4658d0301298a4a813a350104506958897886a71
tomoki497932 <tomoki497932@gmail.com>
2024-03-30 15:56:44 +0900

    f-strings

---
+++ f-strings.py
@@ -0,0 +1,10 @@
+a = 'a'
+print(f'a is {a}')
+ 
+x, y, z = 1, 2, 3
+print(f'a is {x}, {y}, {z}')
+print(f'a is {z}, {y}, {x}')
+ 
+name = 'Tomoki'
+family = 'Ishikawa'
+print(f'My name is {name} {family}. Watashi ha {family} {name}')
